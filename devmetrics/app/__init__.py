9c418d5122fdc08c06523163ee0505970033a4ea
yashwinreddy <98632853+yashwinreddy@users.noreply.github.com>
2025-08-07 04:53:13 +0000

    adding new files, apparently cut copy paste from vs code doesnt work especially backend

--- devmetrics/app/__init__.py
+++ devmetrics/app/__init__.py
@@ -1,7 +1,17 @@
+from flask_dance.contrib.github import make_github_blueprint, github
+import os
 from flask import Flask
 
 def create_app():
     app = Flask(__name__)
+    app.secret_key = os.getenv("FLASK_SECRET_KEY")
+
+    github_bp = make_github_blueprint(
+        client_id=os.getenv('Ov23liXZ4qDunIrxjL2V'),
+        client_secret=os.getenv('16feb4d17754f8ecbbbc062bb8bc6fd0d22a9587'),
+    )
+    app.register_blueprint(github_bp, url_prefix="/github_login")
+
     from .routes import main
     app.register_blueprint(main)
     return app
--- devmetrics/app/routes.py
+++ devmetrics/app/routes.py
@@ -1,7 +1,19 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, redirect, url_for
+from flask_dance.contrib.github import github
+
 
 main = Blueprint('main', __name__)
 
 @main.route('/')
 def index():
-    return render_template('index.html')
+    if not github.authorized:
+        return redirect(url_for('github.login'))
+    resp= github.get('/user')
+    assert resp.ok, resp.text
+    github_info = resp.json()
+    username = github_info['login']
+
+    repos_resp = github.get('/user/repos')
+    repos = repos_resp.json() if repos_resp.ok else []
+
+    return render_template('index.html', username=username, repos=repos)
